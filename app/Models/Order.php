0ba25b0a3651eaab93649e5ebad1151edc58e6f6
Shreyan-Shrestha <shreyan.freelance@gmail.com>
2026-04-28 10:40:29 +0545

    Created migrations and models for Orders table

---
+++ app/Models/Order.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $primaryKey = 'customer_id';
+
+    protected $fillable = [
+        'customer_name',
+        'order_amount',
+    ];
+
+    protected $casts = [
+        'order_amount' => 'integer',
+    ];
+}
